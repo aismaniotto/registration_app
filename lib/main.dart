7923419333dea33453ff585da064d47b426c9db7
aismaniotto <aismaniotto@gmail.com>
2021-09-16 21:19:50 -0300

    feature: add getX package and refactor default project

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(GetMaterialApp(home: Home()));
+
+class Controller extends GetxController {
+  var count = 0.obs;
+  increment() => count++;
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class Home extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+  Widget build(context) {
+    // Instantiate your class using Get.put() to make it available for all "child" routes there.
+    final Controller c = Get.put(Controller());
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        // Use Obx(()=> to update Text() whenever count is changed.
+        appBar: AppBar(title: Obx(() => Text("Clicks: ${c.count}"))),
+
+        // Replace the 8 lines Navigator.push by a simple Get.to(). You don't need context
+        body: Center(
+            child: ElevatedButton(
+                child: const Text("Go to Other"),
+                onPressed: () => Get.to(Other()))),
+        floatingActionButton: FloatingActionButton(
+            child: const Icon(Icons.add), onPressed: c.increment));
+  }
+}
+
+class Other extends StatelessWidget {
+  // You can ask Get to find a Controller that is being used by another page and redirect you to it.
+  final Controller c = Get.find();
+
+  @override
+  Widget build(context) {
+    // Access the updated count variable
+    return Scaffold(body: Center(child: Text("${c.count}")));
   }
 }
